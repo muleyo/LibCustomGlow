9d44c4dde5d229f3791fba173b38740f0230f20f
MoONSHO7 <benmoon88@gmail.com>
2026-01-25 23:41:08 +0800

    12.0.0 Curve Capability (#32)
    * 12.0.0 Curve Capability
    
    * - Increment minor
    - Remove comments to align with project

--- LibCustomGlow-1.0.lua
+++ LibCustomGlow-1.0.lua
@@ -6,7 +6,7 @@ https://www.wowace.com/projects/libbuttonglow-1-0
 -- luacheck: globals CreateFromMixins ObjectPoolMixin CreateTexturePool CreateFramePool
 
 local MAJOR_VERSION = "LibCustomGlow-1.0"
-local MINOR_VERSION = 21
+local MINOR_VERSION = 22
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
 local lib, oldversion = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then return end
@@ -151,7 +151,12 @@ local function addFrameAndTex(r,color,name,key,N,xOffset,yOffset,texture,texCoor
                 f.textures[i]:SetBlendMode("ADD")
             end
         end
-        f.textures[i]:SetVertexColor(color[1],color[2],color[3],color[4])
+        -- Handle both array format {r,g,b,a} and Color objects (for WoW 12.0 secret values)
+        if type(color) == "table" and color.GetRGBA then
+            f.textures[i]:SetVertexColor(color:GetRGBA())
+        else
+            f.textures[i]:SetVertexColor(color[1],color[2],color[3],color[4])
+        end
         f.textures[i]:Show()
     end
     while #f.textures>N do
@@ -685,7 +690,12 @@ function lib.ButtonGlow_Start(r,color,frequency,frameLevel)
         else
             for texture in pairs(ButtonGlowTextures) do
                 f[texture]:SetDesaturated(1)
-                f[texture]:SetVertexColor(color[1],color[2],color[3])
+                if type(color) == "table" and color.GetRGBA then
+                    local r, g, b = color:GetRGBA()
+                    f[texture]:SetVertexColor(r, g, b)
+                else
+                    f[texture]:SetVertexColor(color[1],color[2],color[3])
+                end
                 local alpha = math.min(f[texture]:GetAlpha()/noZero(f.color and f.color[4] or 1)*color[4], 1)
                 f[texture]:SetAlpha(alpha)
                 updateAlphaAnim(f,color and color[4] or 1)
@@ -717,7 +727,12 @@ function lib.ButtonGlow_Start(r,color,frequency,frameLevel)
             f.color = color
             for texture in pairs(ButtonGlowTextures) do
                 f[texture]:SetDesaturated(1)
-                f[texture]:SetVertexColor(color[1],color[2],color[3])
+                if type(color) == "table" and color.GetRGBA then
+                    local r, g, b = color:GetRGBA()
+                    f[texture]:SetVertexColor(r, g, b)
+                else
+                    f[texture]:SetVertexColor(color[1],color[2],color[3])
+                end
             end
         end
         f.throttle = throttle
